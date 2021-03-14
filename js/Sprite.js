632d8fbe5419227e3b8e3a256799cd4e9db0fdf7
Raquel Goulart <raquelgoulart@ice.ufjf.br>
2021-03-14 17:10:08 -0300

    Refatora restrições para caixa perfeita

--- js/Sprite.js
+++ js/Sprite.js
@@ -51,13 +51,21 @@ export default class Sprite{
 
     aplicaRestricoes(dt){
         
-        this.aplicaRestricoesDireita(dt);
-        this.aplicaRestricoesEsquerda(dt);   
-        this.aplicaRestricoesBaixo(dt);
-        this.aplicaRestricoesCima(dt);
+        this.aplicaRestricoesDireita(this.mx + 1, this.my - 1);
+        this.aplicaRestricoesDireita(this.mx + 1, this.my);
+        this.aplicaRestricoesDireita(this.mx + 1, this.my + 1);
+        this.aplicaRestricoesEsquerda(this.mx - 1, this.my - 1);   
+        this.aplicaRestricoesEsquerda(this.mx - 1, this.my);   
+        this.aplicaRestricoesEsquerda(this.mx - 1, this.my + 1);   
+        this.aplicaRestricoesBaixo(this.mx - 1, this.my + 1);
+        this.aplicaRestricoesBaixo(this.mx, this.my + 1);
+        this.aplicaRestricoesBaixo(this.mx + 1, this.my + 1);
+        this.aplicaRestricoesCima(this.mx - 1, this.my + 1);
+        this.aplicaRestricoesCima(this.mx, this.my + 1);
+        this.aplicaRestricoesCima(this.mx + 1, this.my + 1);
     }
 
-    aplicaRestricoesDireita(dt) {
+    aplicaRestricoesDireita(pmx, pmy) {
         const SIZE = this.cena.mapa.SIZE;
         if(this.vx > 0){
             const pmx = this.mx+1;
@@ -76,7 +84,7 @@ export default class Sprite{
          }
     }
 
-    aplicaRestricoesEsquerda(dt) {
+    aplicaRestricoesEsquerda(pmx, pmy) {
         const SIZE = this.cena.mapa.SIZE;
         if(this.vx < 0){
             const pmx = this.mx-1;
@@ -95,7 +103,7 @@ export default class Sprite{
          }
     }
 
-    aplicaRestricoesBaixo(dt) {
+    aplicaRestricoesBaixo(pmx, pmy) {
         const SIZE = this.cena.mapa.SIZE;
         if(this.vy > 0){
             const pmx = this.mx;
@@ -114,7 +122,7 @@ export default class Sprite{
          }
     }
 
-    aplicaRestricoesCima(dt) {
+    aplicaRestricoesCima(pmx, pmy) {
         const SIZE = this.cena.mapa.SIZE;
         if(this.vy < 0){
             const pmx = this.mx;
