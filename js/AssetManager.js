9e4629e77731cb84d5aabb4aa7b9a385c5b1c0ca
Raquel Goulart <raquelgoulart@ice.ufjf.br>
2021-03-14 13:45:48 -0300

    Add um método acabou

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -3,12 +3,19 @@ export default class AssetManager {
         this.aCarregar = 0;
         this.carregadas = 0;
         this.imagens = new Map();
-    }
+            }
 
     carregaImagem(chave, source){
         const img1 = new Image();
+        img1.addEventListener("load", () =>{
+            console.log(`Imagem ${this.carregadas}/${this.aCarregar} carregada!`);
+            this.carregadas++;
+
+        });
+
         img1.src = source;
         this.imagens.set(chave, img1);
+        this.aCarregar++;
             
     }
 
@@ -22,4 +29,8 @@ export default class AssetManager {
         }
         return "Nada a Carregar";
     }
+
+    acabou()[
+        return this.carregadas === this.aCarregar;
+    ]
 }
