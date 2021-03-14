5efd23b9df573cab76246c66d6236cd519aa9a18
Raquel Goulart <raquelgoulart@ice.ufjf.br>
2021-03-14 13:56:29 -0300

    Add carregamento de audio

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -3,7 +3,8 @@ export default class AssetManager {
         this.aCarregar = 0;
         this.carregadas = 0;
         this.imagens = new Map();
-            }
+        this.audios = new Map()
+    }
 
     carregaImagem(chave, source){
         const img1 = new Image();
@@ -12,15 +13,33 @@ export default class AssetManager {
             this.carregadas++;
 
         });
-
         img1.src = source;
         this.imagens.set(chave, img1);
         this.aCarregar++;
             
     }
+    
 
     img(chave){
         return this.imagens.get(chave)
+    }
+
+
+    carregaAudio(chave, source){
+        const audio = new Audio();
+        audio.addEventListener("canplay", () =>{
+            console.log(`Audio ${this.carregadas}/${this.aCarregar} carregado!`);
+            this.carregadas++;
+
+        });
+        audio.src = source;
+        this.audios.set(chave, audio);
+        this.aCarregar++;
+            
+    }
+    
+    audio(chave){
+        return this.audios.get(chave)
     }
 
     progresso(){
--- js/Main.js
+++ js/Main.js
@@ -7,6 +7,7 @@ const assets = new AssetManager();
 assets.carregaImagem ("garota", "assets/girl.png");
 assets.carregaImagem ("esqueleto", "assets/skelly.png");
 assets.carregaImagem ("orc", "assets/orc.png");
+assets.carregaAudio ("moeda", "assets/coin.wav");
 
 document.body.appendChild(assets.img("garota"));
 document.body.appendChild(assets.img("esqueleto"));
