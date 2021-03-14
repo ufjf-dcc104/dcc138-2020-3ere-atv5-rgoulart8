b72482855d272dbef3d8413cb511e78a229777f1
Raquel Goulart <raquelgoulart@ice.ufjf.br>
2021-03-14 19:30:53 -0300

    Add evento de audio quando sprites colidirem

--- js/Main.js
+++ js/Main.js
@@ -11,20 +11,24 @@ const assets = new AssetManager(mixer);
 assets.carregaImagem ("garota", "assets/girl.png");
 assets.carregaImagem ("esqueleto", "assets/skelly.png");
 assets.carregaImagem ("orc", "assets/orc.png");
+assets.carregaImagem ("grama", "assets/grama.png");
+assets.carregaImagem ("tijolo", "assets/brick.png");
+assets.carregaImagem ("agua", "assets/water.png");
 assets.carregaAudio ("moeda", "assets/coin.wav");
 assets.carregaAudio ("explosao", "assets/boom.wav");
 
 const canvas = document.querySelector("canvas");
-canvas.width = 14*32;
-canvas.height = 10*32;
+canvas.width = 25*32;
+canvas.height = 20*32;
 const cena1 = new Cena(canvas, assets);
 
-const mapa1 = new Mapa(10, 14, 32);
+const mapa1 = new Mapa(20, 25, 32);
 mapa1.carregaMapa(modeloMapa1)
 cena1.configuraMapa(mapa1);
 
 const pc = new Sprite({x: 50, vx:10});
-const en1 = new Sprite({x:160, vx: -10, collor:"red"});
+const en1 = new Sprite({x:200, vx: -10, collor:"red"});
+
 
 cena1.adicionar(pc);
 cena1.adicionar(en1);
@@ -49,3 +53,5 @@ document.addEventListener("keydown", (e)=>{
         break;
     }
     });
+
+
