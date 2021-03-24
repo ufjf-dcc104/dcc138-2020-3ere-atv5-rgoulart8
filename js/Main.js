f60af9943f91a176a18b90a5767b9723ad2e85d6
Raquel Goulart <raquelgoulart@ice.ufjf.br>
2021-03-24 15:50:27 -0300

    Add acesso a iniciar/parar cena via game

--- js/Main.js
+++ js/Main.js
@@ -92,15 +92,15 @@ cena1.quandoCriar = function(){
           
 
 
-cena1.iniciar();
+game.iniciar();
 
 document.addEventListener("keydown", (e)=>{
     switch (e.key) {
     case "s":
-        cena1.iniciar();
+        game.iniciar();
         break;
     case "S":
-        cena1.parar();
+        game.parar();
         break;
     case "c":
         assets.play("moeda");
