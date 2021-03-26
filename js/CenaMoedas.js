fcd68ee0c5c0d6865431bf033234becb8bb4e979
Raquel Goulart <raquelgoulart@ice.ufjf.br>
2021-03-25 22:05:22 -0300

    Corrige audio de pega de moeda

--- js/CenaMoedas.js
+++ js/CenaMoedas.js
@@ -3,82 +3,91 @@ import Mapa from "./Mapa.js";
 import Sprite from "./Sprite.js";
 import modeloMapa1 from "../maps/mapa2.js";
 
-export default class CenaMoedas extends Cena{
-
-    quandoColidir (a, b){
-        let contador = 0;
-       this.assets.playColidiu("coin");
-        if (a.tags.has("pc") && b.tags.has("moeda")){
-            contador ++;
-            if(!this.aRemover.includes(b)){
-                this.aRemover.push(b);
-            }
-        }
-        if (a.tags.has("pc") && b.tags.has("muda")){
-                this.rodando = false;
-                this.game.selecionaCena("jogo");
-        }
-       
+export default class CenaMoedas extends Cena {
+  quandoColidir(a, b) {
+    let contador = 0;
+    if (a.tags.has("pc") && b.tags.has("moeda")) {
+      this.assets.play("moeda");
+      contador++;
+      if (!this.aRemover.includes(b)) {
+        this.aRemover.push(b);
+      }
     }
+    if (a.tags.has("pc") && b.tags.has("muda")) {
+      this.rodando = false;
+      this.game.selecionaCena("jogo");
+    }
+  }
 
-    preparar(){
-        super.preparar();
-        const mapa2 = new Mapa(20, 25, 32);
-        mapa2.carregaMapa(modeloMapa1);
-        this.configuraMapa(mapa2);
+  preparar() {
+    super.preparar();
+    const mapa2 = new Mapa(20, 25, 32);
+    mapa2.carregaMapa(modeloMapa1);
+    this.configuraMapa(mapa2);
 
-        const pc = new Sprite({x: 50, y: 120});
-        pc.tags.add("pc");
-        const cena = this;
-        pc.controlar = function(dt){
-         if(cena.input.comandos.get("MOVE_ESQUERDA")){
+    const pc = new Sprite({ x: 50, y: 120 });
+    pc.tags.add("pc");
+    const cena = this;
+    pc.controlar = function (dt) {
+      if (cena.input.comandos.get("MOVE_ESQUERDA")) {
         this.vx = -50;
-    }
-         else if(cena.input.comandos.get("MOVE_DIREITA")){
+      } else if (cena.input.comandos.get("MOVE_DIREITA")) {
         this.vx = +50;
-    }
-          else{
+      } else {
         this.vx = 0;
-    }
-          if(cena.input.comandos.get("MOVE_CIMA")){
+      }
+      if (cena.input.comandos.get("MOVE_CIMA")) {
         this.vy = -50;
-    }
-          else if(cena.input.comandos.get("MOVE_BAIXO")){
+      } else if (cena.input.comandos.get("MOVE_BAIXO")) {
         this.vy = +50;
-    }
-          else{
+      } else {
         this.vy = 0;
+      }
+    };
+    this.adicionar(pc);
+
+    function perseguePC(dt) {
+      this.vx = 25 * Math.sign(pc.x - this.x);
+      this.vy = 25 * Math.sign(pc.y - this.y);
     }
-}
-        this.adicionar(pc);
+    const en1 = new Sprite({
+      x: 360,
+      color: "red",
+      controlar: perseguePC,
+      tags: ["muda"],
+    });
+    en1.controlar = perseguePC;
 
-        function perseguePC(dt){
-         this.vx = 25*Math.sign(pc.x - this.x);
-         this.vy = 25*Math.sign(pc.y - this.y);
+    this.adicionar(en1);
+    this.adicionar(
+      new Sprite({ x: 115, y: 70, color: "yellow", tags: ["moeda"] })
+    );
+    this.adicionar(
+      new Sprite({ x: 650, y: 70, color: "yellow", tags: ["moeda"] })
+    );
+    this.adicionar(
+      new Sprite({ x: 150, y: 180, color: "yellow", tags: ["moeda"] })
+    );
+    this.adicionar(
+      new Sprite({ x: 315, y: 299, color: "yellow", tags: ["moeda"] })
+    );
 
-}
-        const en1 = new Sprite({x:360, color:"red", controlar: perseguePC, tags:["muda"]});
-        en1.controlar = perseguePC;
-    
-        this.adicionar(en1);
-        this.adicionar(new Sprite({x: 115, y: 70, color: "yellow", tags:["moeda"]}));
-        this.adicionar(new Sprite({x: 650, y: 70, color: "yellow", tags:["moeda"]}));
-        this.adicionar(new Sprite({x: 150, y: 180, color: "yellow", tags:["moeda"]}));
-        this.adicionar(new Sprite({x: 315, y: 299, color: "yellow", tags:["moeda"]}));
+    this.quandoCriar = function () {
+      let nmy = Math.floor(Math.random() * (this.mapa.tiles.length - 2)) + 1;
+      let nmx = Math.floor(Math.random() * (this.mapa.tiles[0].length - 2)) + 1;
 
-
-        this.quandoCriar = function(){
-            let nmy = Math.floor(Math.random()*(this.mapa.tiles.length-2))+1;
-            let nmx = Math.floor(Math.random()*(this.mapa.tiles[0].length-2))+1;
-        
-        while(this.mapa.tiles[nmy][nmx] != 0){
-            nmy = Math.floor(Math.random()*(this.mapa.tiles.length-2))+1;
-            nmx = Math.floor(Math.random()*(this.mapa.tiles[0].length-2))+1;
-        }
-            this.adicionar(new Sprite({x: nmx*this.mapa.SIZE+this.mapa.SIZE/2, 
-                y: nmy*this.mapa.SIZE+this.mapa.SIZE/2, color: "yellow", tags:["moeda"]}));
-                        
-            }
-    }
-
+      while (this.mapa.tiles[nmy][nmx] != 0) {
+        nmy = Math.floor(Math.random() * (this.mapa.tiles.length - 2)) + 1;
+        nmx = Math.floor(Math.random() * (this.mapa.tiles[0].length - 2)) + 1;
+      }
+      this.adicionar(
+        new Sprite({
+          x: nmx * this.mapa.SIZE + this.mapa.SIZE / 2,
+          y: nmy * this.mapa.SIZE + this.mapa.SIZE / 2,
+          color: "yellow",
+          tags: ["moeda"],
+        })
+      );
+    };
+  }
 }
