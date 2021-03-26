28513ef419f72c6f9ee17e3bb57183cc3776d359
Raquel Goulart <raquelgoulart@ice.ufjf.br>
2021-03-26 12:37:40 -0300

    Remove progresso de carregamento da Cena de jogo

--- js/Cena.js
+++ js/Cena.js
@@ -20,8 +20,7 @@ export default class Cena {
             sprite.desenhar(this.ctx);
             sprite.aplicaRestricoes();
         }}
-        this.ctx.fillStyle = "yellow"
-        this.ctx.fillText(this.assets?.progresso(), 10, 20);
+        
     }
 
     adicionar(sprite){
