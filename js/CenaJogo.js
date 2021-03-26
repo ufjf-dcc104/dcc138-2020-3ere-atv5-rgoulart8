ba1d8bfb0cc98294646b337cb08f0fd620a90421
Raquel Goulart <raquelgoulart@ice.ufjf.br>
2021-03-25 22:05:06 -0300

    Corrige bug de colisao com muda de fase

--- js/CenaJogo.js
+++ js/CenaJogo.js
@@ -3,86 +3,95 @@ import Mapa from "./Mapa.js";
 import Sprite from "./Sprite.js";
 import modeloMapa1 from "../maps/mapa1.js";
 
-export default class CenaJogo extends Cena{
-
-    quandoColidir (a, b){
-        if(!this.aRemover.includes(a) ){
+export default class CenaJogo extends Cena {
+  quandoColidir(a, b) {
+    /* if(!this.aRemover.includes(a) ){
             this.aRemover.push(a);
         }
         if(!this.aRemover.includes(b)){
             this.aRemover.push(b);
-        }
-        
-        if (a.tags.has("pc") && b.tags.has("enemy")){
-            this.assets.playColidiu("explosao");
-            this.rodando = false;
-            this.game.selecionaCena("fim");
-        if (a.tags.has("pc") && b.tags.has("muda")){
-            this.assets.playColidiu("coin");
-                this.rodando = false;
-                this.game.selecionaCena("moedas");
-        }}
-       
+        }*/
+
+    if (a.tags.has("pc") && b.tags.has("enemy")) {
+      this.assets.play("explosao");
+      this.rodando = false;
+      this.game.selecionaCena("fim");
     }
 
-    preparar(){
-        super.preparar();
-        const mapa1 = new Mapa(20, 25, 32);
-        mapa1.carregaMapa(modeloMapa1);
-        this.configuraMapa(mapa1);
+    if (
+      (a.tags.has("pc") && b.tags.has("muda")) ||
+      (b.tags.has("pc") && a.tags.has("muda"))
+    ) {
+      this.assets.play("moeda");
+      this.rodando = false;
+      this.game.selecionaCena("moedas");
+    }
+  }
 
-        const pc = new Sprite({x: 50, y: 120});
-        pc.tags.add("pc");
-        const cena = this;
-        pc.controlar = function(dt){
-         if(cena.input.comandos.get("MOVE_ESQUERDA")){
+  preparar() {
+    super.preparar();
+    const mapa1 = new Mapa(20, 25, 32);
+    mapa1.carregaMapa(modeloMapa1);
+    this.configuraMapa(mapa1);
+
+    const pc = new Sprite({ x: 50, y: 120 });
+    pc.tags.add("pc");
+    const cena = this;
+    pc.controlar = function (dt) {
+      if (cena.input.comandos.get("MOVE_ESQUERDA")) {
         this.vx = -50;
-    }
-         else if(cena.input.comandos.get("MOVE_DIREITA")){
+      } else if (cena.input.comandos.get("MOVE_DIREITA")) {
         this.vx = +50;
-    }
-          else{
+      } else {
         this.vx = 0;
-    }
-          if(cena.input.comandos.get("MOVE_CIMA")){
+      }
+      if (cena.input.comandos.get("MOVE_CIMA")) {
         this.vy = -50;
-    }
-          else if(cena.input.comandos.get("MOVE_BAIXO")){
+      } else if (cena.input.comandos.get("MOVE_BAIXO")) {
         this.vy = +50;
-    }
-          else{
+      } else {
         this.vy = 0;
+      }
+    };
+    this.adicionar(pc);
+
+    function perseguePC(dt) {
+      this.vx = 25 * Math.sign(pc.x - this.x);
+      this.vy = 25 * Math.sign(pc.y - this.y);
     }
-}
-        this.adicionar(pc);
+    const en1 = new Sprite({
+      x: 360,
+      color: "red",
+      controlar: perseguePC,
+      tags: ["enemy"],
+    });
+    en1.controlar = perseguePC;
 
-        function perseguePC(dt){
-         this.vx = 25*Math.sign(pc.x - this.x);
-         this.vy = 25*Math.sign(pc.y - this.y);
-
-}
-        const en1 = new Sprite({x:360, color:"red", controlar: perseguePC, tags:["enemy"]});
-        en1.controlar = perseguePC;
-    
-        this.adicionar(en1);
-        this.adicionar(new Sprite({x: 400, y: 500, color: "yellow", tags:["muda"]}));
-        /*cena1.adicionar(new Sprite({x: 650, y: 70, vx: -10, color: "pink", controlar: perseguePC}));
+    this.adicionar(en1);
+    this.adicionar(
+      new Sprite({ x: 150, y: 200, color: "yellow", tags: ["muda"] })
+    );
+    /*cena1.adicionar(new Sprite({x: 650, y: 70, vx: -10, color: "pink", controlar: perseguePC}));
         cena1.adicionar(new Sprite({x: 150, y: 180, vy: -10, color: "green", controlar: perseguePC}));
         cena1.adicionar(new Sprite({x: 315, y: 299, vy: +10, color: "blue",controlar: perseguePC}));*/
 
+    this.quandoCriar = function () {
+      let nmy = Math.floor(Math.random() * (this.mapa.tiles.length - 2)) + 1;
+      let nmx = Math.floor(Math.random() * (this.mapa.tiles[0].length - 2)) + 1;
 
-        this.quandoCriar = function(){
-            let nmy = Math.floor(Math.random()*(this.mapa.tiles.length-2))+1;
-            let nmx = Math.floor(Math.random()*(this.mapa.tiles[0].length-2))+1;
-        
-        while(this.mapa.tiles[nmy][nmx] != 0){
-            nmy = Math.floor(Math.random()*(this.mapa.tiles.length-2))+1;
-            nmx = Math.floor(Math.random()*(this.mapa.tiles[0].length-2))+1;
-        }
-            this.adicionar(new Sprite({x: nmx*this.mapa.SIZE+this.mapa.SIZE/2, 
-                y: nmy*this.mapa.SIZE+this.mapa.SIZE/2, controlar: perseguePC, color: "red", tags: ["enemy"]}));
-                        
-            }
-    }
-
+      while (this.mapa.tiles[nmy][nmx] != 0) {
+        nmy = Math.floor(Math.random() * (this.mapa.tiles.length - 2)) + 1;
+        nmx = Math.floor(Math.random() * (this.mapa.tiles[0].length - 2)) + 1;
+      }
+      this.adicionar(
+        new Sprite({
+          x: nmx * this.mapa.SIZE + this.mapa.SIZE / 2,
+          y: nmy * this.mapa.SIZE + this.mapa.SIZE / 2,
+          controlar: perseguePC,
+          color: "red",
+          tags: ["enemy"],
+        })
+      );
+    };
+  }
 }
