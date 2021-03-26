f66dd2614610e5c7970f612f2260dc72f1d4462b
Raquel Goulart <raquelgoulart@ice.ufjf.br>
2021-03-25 22:04:40 -0300

    Remove método duplicado

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -1,65 +1,56 @@
 export default class AssetManager {
-    constructor(mixer=null){
-        this.aCarregar = 0;
-        this.carregadas = 0;
-        this.imagens = new Map();
-        this.audios = new Map()
-        this.mixer = mixer;
-    }
+  constructor(mixer = null) {
+    this.aCarregar = 0;
+    this.carregadas = 0;
+    this.imagens = new Map();
+    this.audios = new Map();
+    this.mixer = mixer;
+  }
 
-    carregaImagem(chave, source){
-        const img1 = new Image();
-        img1.addEventListener("load", () =>{
-            this.carregadas++;
+  carregaImagem(chave, source) {
+    const img1 = new Image();
+    img1.addEventListener("load", () => {
+      this.carregadas++;
+    });
+    img1.src = source;
+    this.imagens.set(chave, img1);
+    this.aCarregar++;
+  }
 
-        });
-        img1.src = source;
-        this.imagens.set(chave, img1);
-        this.aCarregar++;
-            
-    }
-    
-    img(chave){
-        return this.imagens.get(chave)
-    }
+  img(chave) {
+    return this.imagens.get(chave);
+  }
 
+  carregaAudio(chave, source) {
+    const audio = new Audio();
+    audio.addEventListener("loadeddata", () => {
+      this.carregadas++;
+    });
+    audio.src = source;
+    this.audios.set(chave, audio);
+    this.aCarregar++;
+  }
 
-    carregaAudio(chave, source){
-        const audio = new Audio();
-        audio.addEventListener("loadeddata", () =>{
-            this.carregadas++;
+  audio(chave) {
+    return this.audios.get(chave);
+  }
 
-        });
-        audio.src = source;
-        this.audios.set(chave, audio);
-        this.aCarregar++;
-            
+  progresso() {
+    if (this.aCarregar > 0) {
+      return `${((this.carregadas / this.aCarregar) * 100).toFixed(2)}%`;
     }
-    
-    audio(chave){
-        return this.audios.get(chave)
-    }
+    return "Nada a Carregar";
+  }
 
-    progresso(){
-        if(this.aCarregar>0){
-            return `${(this.carregadas/this.aCarregar*100).toFixed(2)}%`;
-        }
-        return "Nada a Carregar";
-    }
+  acabou() {
+    return this.carregadas === this.aCarregar;
+  }
 
-    acabou(){
-        return this.carregadas === this.aCarregar;
-    }
-    
-    play(chave){
-        this.mixer?.play(this.audio(chave));
-    }
+  play(chave) {
+    this.mixer?.play(this.audio(chave));
+  }
 
-    playColidiu(chave){
-        this.mixer?.play(this.audio(chave));
-    }
-
-    desenhaImg(chave){
-        this.imagens.load(chave);
-    }
+  desenhaImg(chave) {
+    this.imagens.load(chave);
+  }
 }
